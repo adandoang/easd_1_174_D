ad5d06640865fabecf8548d0624c87a998202438
adandoang <akhdanjzif@gmail.com>
2023-04-07 09:47:49 +0700

    jawaban 4

--- easd_1_174_D/easd_1_174_D.cpp
+++ easd_1_174_D/easd_1_174_D.cpp
@@ -1,3 +1,4 @@
 // 1. karena algoritma adalah urutan instruksi yang terstruktur dan terorganisir yang mengikuti logika atau langkah-langkah tertentu untuk menyelesaikan suatu masalah atau mencapai suatu tujuan
 // 2. struktur statik dan dinamik
 // 3. penggunaan memori, jumlah input data, jumlah operasi, jenis algoritma, kemampuan komputer, bahasa pemrograman
+// 4. insertionsort, karena hanya ada satu perulangan untuk setiap elemen data, sehingga jumlah operasi yang diperlukan untuk mengurutkan data relatif sedikit
