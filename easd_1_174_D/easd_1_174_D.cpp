ed27c32164deb69d709ae45546a61ee4df1f5081
adandoang <akhdanjzif@gmail.com>
2023-04-07 09:46:02 +0700

    jabawan 1

--- easd_1_174_D/easd_1_174_D.cpp
+++ easd_1_174_D/easd_1_174_D.cpp
@@ -1,1 +1,1 @@
-// 1. karena algoritma adalah urutan instruksi yang terstruktur dan terorganisir yang mengikuti logika atau langkah-langkah tertentu untuk menyelesaikan suatu masalah atau mencapai suatu tujuan 
+// 1. karena algoritma adalah urutan instruksi yang terstruktur dan terorganisir yang mengikuti logika atau langkah-langkah tertentu untuk menyelesaikan suatu masalah atau mencapai suatu tujuan
