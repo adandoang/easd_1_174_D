63622291bf2995410ba991759003c12ebbc68bae
adandoang <akhdanjzif@gmail.com>
2023-04-07 09:47:07 +0700

    jawaban 3

--- easd_1_174_D/easd_1_174_D.cpp
+++ easd_1_174_D/easd_1_174_D.cpp
@@ -1,2 +1,3 @@
 // 1. karena algoritma adalah urutan instruksi yang terstruktur dan terorganisir yang mengikuti logika atau langkah-langkah tertentu untuk menyelesaikan suatu masalah atau mencapai suatu tujuan
 // 2. struktur statik dan dinamik
+// 3. penggunaan memori, jumlah input data, jumlah operasi, jenis algoritma, kemampuan komputer, bahasa pemrograman
